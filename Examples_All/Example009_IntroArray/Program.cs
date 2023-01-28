947796aa6455d160718ed1005e230e695231e875
Jane-547 <Jane-547@bk.com>
2023-01-28 13:40:19 +0300

    Добавлены Example008, Example009, Example010, Example011

---
+++ Examples_All/Example009_IntroArray/Program.cs
@@ -0,0 +1,16 @@
+﻿int Max (int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+
+int [] array = { 15, 211, 39, 12, 84, 33, 23, 99, 7 };
+
+int result = Max (
+    Max (array[0], array[1], array[2]),
+    Max (array[3], array[4], array[5]),
+    Max (array[6], array[7], array[8]));
+
+Console.WriteLine(result);
