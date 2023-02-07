41d716c65c93dcc9a61e9a67e4fa5974bba99e5d
Jane-547 <Jane-547@bk.com>
2023-02-07 17:52:03 +0300

    Added several Examples

---
+++ Examples_All/Example013_Functions/Program.cs
@@ -0,0 +1,115 @@
+﻿void Metod1()
+{
+Console.WriteLine("Автор …");
+}
+
+Metod1();
+
+
+
+
+void Metod2(string msg) // где void ключевое слово, дальше идентификатор, в скобках указаны какие-то аргументы.
+{
+Console.WriteLine(msg); // оператор, в скобках указан принятый аргумент.
+}
+
+Metod2(“Текст сообщения”);
+
+
+//
+
+void Metod21(string msg, int count)
+{
+int i = 0;
+while (i < count)
+{
+Console.WriteLine(msg); // где переменная count отображает на экране определённое количество сообщений msg.
+i++; 
+}
+}
+Metod21(“Текст”, 4);
+
+//
+
+int Metod3() // не принимает никакие аргументы
+{
+return DataTime.Now.Year; // обязательное использование оператора return,
+}
+int year = Metod3(); // вызываем метод, в левой части используем идентификатор переменной (year) и через оператор присваивания (=) кладём нужное значение
+Console.WriteLine(year);
+
+//
+
+string Metod4(int count, string text)
+{
+int i = 0;
+string result = String.Empty;
+while (i<count)
+{
+	result = result + text;
+i++;
+	}
+	return result;
+}
+string res = Metod4(10, “asdf”);
+Console.WriteLine(res);
+
+
+//
+
+string Metod4(int count, string text)
+{
+string result = String.Empty;
+for (int i = 0; i<count; i++) // вначале ключевое слово, затем инициализация счетчика, после проверка условия и инкремент (увеличение счетчика).
+{
+	result = result + text;
+}
+return result;
+}
+string res = Metod4(10, “asdf”);
+Console.WriteLine(res);
+
+//
+
+for (int i = 2; i <= 10; i++)
+{
+for (int j = 2; j <= 10; j++)
+{
+Console.WriteLine($”{i} * {j} = {i * j}”);
+}
+Console.WriteLine();
+}
+
+
+//
+
+int[] arr = {1, 5, 4, 3, 2, 6, 7, 1, 1};
+void PtintArray(int[] array)
+{
+int count = array.Length
+for (int i = 0; i < count; i++)
+{
+Console.Write($”{array[i]}”);
+}
+Console.WriteLine();
+}
+void selectionSort(int[] array)
+{
+for (int i = 0; i < array.Length - 1; i++)
+{
+int minPosition = i;
+for (int j = i+1; j < array.Length; j++)
+{
+if(array[j] < array[minPosition]) minPosition = j;
+{
+minPosition = j
+}
+}
+int temporary = array[i]
+array[i] = array[minPosition];
+array[minPosition] = temporary;
+}
+}
+PrintArray(arr);
+SelectionSort(arr);
+PrintArray(arr);
---
+++ Examples_All/Example014_Seminar4/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача 26: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+
+
+// Console.WriteLine("Введите число");
+// int numA = Convert.ToInt32(Console.ReadLine());
+
+// int CountNum (int number)
+// {
+//     int count = 1;
+//     for (; number >= 10; count++)
+//     {
+//         number /= 10;
+//     }
+//     return count;
+// }
+
+// Console.WriteLine(CountNum(numA));
+
+// Задача 28: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+
+System.Console.Write("Enter number N :");
+int numN = int.Parse(Console.ReadLine());
+
+
+
+if (numN < 1)
+{
+    System.Console.WriteLine("Число меньше 1");
+}
+else
+{
+   
+    System.Console.WriteLine($"Произведение =  {GetMult(numN)}");
+}
+
+
+int GetMult(int number)
+{
+    int Mult = 1;
+
+    for (int i = 1; i <= number; i++)
+    {
+        Mult *= i;
+    }
+
+    return Mult;
+}
