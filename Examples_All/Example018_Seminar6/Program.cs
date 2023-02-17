44c2c0ecd17005111539065a2bafddadf399d97e
Jane-547 <Jane-547@bk.com>
2023-02-17 18:11:46 +0300

    Example 019 fdded

---
+++ Examples_All/Example018_Seminar6/Program.cs
@@ -0,0 +1,165 @@
+﻿// Задача 39:
+// Напишите программу, которая перевернёт
+// одномерный массив (последний элемент будет на первом
+// месте, а первый на последнем и т.д.)
+// [1 2 3 4 5]
+// --> [5 4 3 2
+// [6 7 3 6]
+// --> [6 3 7
+
+
+int EnterNumber(string number)
+{
+    Console.Write($"Enter {number}: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+
+
+int [] CreateArray(int size, int minValue, int maxValue)
+{
+    int [] res = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return res;
+}
+
+int ArraySize = EnterNumber("Enter array size");
+int minValue = EnterNumber("Enter minValue for array");
+int maxValue = EnterNumber("Enter maxValue for array");
+int Result = 0;
+Console.WriteLine();
+
+int [] numbers = CreateArray(ArraySize, minValue, maxValue);
+
+Console.WriteLine(String.Join(" ", numbers));
+Console.WriteLine();
+
+Console.WriteLine(String.Join(" ", ReversArray(numbers)));
+
+int [] ReversArray (int [] set)
+{
+    
+    for (int i = 0; i < set.Length/2; i++)
+    {
+    int temp = set [i];
+    set[i] = set [set.Length - 1 - i];
+    set [set.Length - 1 - i] = temp;
+    }
+    return set;
+}
+
+/*
+Напишите программу, которая перевернёт
+одномерный массив (последний элемент будет на первом
+месте, а первый на последнем и т.д.)
+[1 2 3 4 5]
+--> [5 4 3 2
+[6 7 3 6]
+--> [6 3 7
+*/
+
+#nullable disable
+
+System.Console.WriteLine("Enter array legth: ");
+int length = int.Parse(Console.ReadLine());
+
+System.Console.WriteLine("Enter array minimal: ");
+int min = int.Parse(Console.ReadLine());
+
+System.Console.WriteLine("Enter array maximal: ");
+int max = int.Parse(Console.ReadLine());
+
+int[] MyArray = GetArray(length, min, max);
+
+System.Console.WriteLine($"[{string.Join(", ", MyArray)}]");
+
+int[] GetArray (int size, int minValue, int maxValue)
+{
+    Random rnd = new Random();
+    int[] result = new int[size];
+    for(int i = 0; i < result.Length; i++)
+    {
+        result[i] = rnd.Next(minValue, maxValue+ 1);        
+    }
+    return result;
+}
+
+Array.Reverse(MyArray);
+
+System.Console.WriteLine($"[{string.Join(", ", MyArray)}]");
+
+Решение в группах задач:
+Задача 40:
+Напишите программу, которая принимает на вход три
+числа и проверяет, может ли существовать треугольник с сторонами
+такой длины.
+Теорема о неравенстве треугольника: каждая сторона треугольника
+меньше суммы двух других сторон.
+
+/*
+Напишите программу, которая принимает на вход три
+числа и проверяет, может ли существовать треугольник с сторонами
+такой длины.
+Теорема о неравенстве треугольника: каждая сторона треугольника
+меньше суммы двух других сторон.
+*/
+
+#nullable disable
+
+System.Console.WriteLine("enter coordinates point A ");
+int A = int.Parse(Console.ReadLine());
+
+System.Console.WriteLine("enter coordinates point B");
+int B = int.Parse(Console.ReadLine());
+
+System.Console.WriteLine("enter coordinates point A");
+int C = int.Parse(Console.ReadLine());
+
+bool IsTriangle(int a,int b,int c)
+{
+    return (((a+b)>c)&&((a+c)>b)&&((b+c)>a));
+}
+if  (IsTriangle(A, B, C))
+{
+    System.Console.WriteLine("Can exist ");
+}
+else
+{
+    System.Console.WriteLine("Impissible ");
+}
+
+Задача 42:
+Напишите программу, которая будет преобразовывать
+десятичное число в двоичное.
+45
+--> 101101
+3
+--> 11
+2
+--> 10
+20 мин
+
+Console.WriteLine("Введите число ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int[]Numbers(int num)
+{
+    int size = 32;
+    int [] result = new int [size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i]= num % 2;
+        num /=2;
+    }
+    return result;
+}
+int[] Binary = (Numbers(number));
+Array.Reverse(Binary);
+Console.WriteLine("Полученный массив = "+ String.Join(" ", Binary));
+
+split
---
+++ Examples_All/Example019_Seminar7/Program.cs
@@ -0,0 +1,246 @@
+﻿Задача 46: Задайте двумерный массив размером m×n, заполненный случайными целыми числами.
+
+int numRows = SetNumber("Rows ");
+int numColumns = SetNumber("Columns ");
+
+int numMaxValue = SetNumber("Max number ");
+int numMinValue = SetNumber("Min number ");
+
+var matrix = GetMatrix(numRows, numColumns, numMinValue, numMaxValue);
+Print(matrix);
+
+int SetNumber(string numberName)
+{
+    Console.Write($"Enter number {numberName}: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+int [,] GetMatrix (int rows, int columns, int min, int max)
+{
+    int [,] matrix = new int [rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int l = 0; l < columns; l++)
+        {
+            matrix [i, l] = new Random().Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void Print(int [ , ] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int l = 0; l < matrix.GetLength(1); l++)
+        {
+            System.Console.Write(matrix[i, l] + " ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+
+Задача 48: Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле: Aₘₙ = m+n. Выведите полученный массив на экран.
+m = 3, n = 4.
+0 1 2 3
+1 2 3 4
+2 3 4 5
+
+int numRows = SetNumber("Rows");
+
+int numColumns = SetNumber("Columns");
+
+
+var matrix = GetMatrix(numRows, numColumns );
+Print(matrix);
+
+int SetNumber(string numberName)
+{
+    Console.Write($"Enter number {numberName}: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+
+int[,] GetMatrix(int rows, int columns)
+{
+    int[,] matrix = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int l = 0; l < columns; l++)
+        {
+            matrix[i, l] = i + l;
+        }
+    }
+    return matrix;
+}
+
+void Print(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int l = 0; l < matrix.GetLength(1); l++)
+        {
+            System.Console.Write(matrix[i, l] + " ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+Задача 49: Задайте двумерный массив. Найдите элементы, у которых оба индекса чётные, и замените эти элементы на их квадраты.
+
+int numRows = SetNumber("Rows ");
+int numColumns = SetNumber("Columns ");
+
+int numMaxValue = SetNumber("Max number ");
+int numMinValue = SetNumber("Min number ");
+
+var matrix = GetMatrix(numRows, numColumns, numMinValue, numMaxValue);
+Print(matrix);
+CheckValue(matrix);
+System.Console.WriteLine();
+Print(matrix);
+
+int SetNumber(string numberName)
+{
+    Console.Write($"Enter number {numberName}: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+int [,] GetMatrix (int rows, int columns, int min, int max)
+{
+    int [,] matrix = new int [rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int l = 0; l < columns; l++)
+        {
+            matrix [i, l] = new Random().Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void Print(int [ , ] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int l = 0; l < matrix.GetLength(1); l++)
+        {
+            System.Console.Write(matrix[i, l] + " ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+void CheckValue(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int l = 0; l < matrix.GetLength(1); l++)
+        {
+            if (isEven(i, l))
+             {
+                matrix[i,l] *= matrix[i,l];
+             }
+        }
+       
+    }
+}
+
+bool isEven (int i, int l) 
+{
+    return i%2 == 0 && l%2 == 0;
+}
+
+
+Задача 51: Задайте двумерный массив. Найдите сумму элементов, находящихся на главной диагонали (с индексами (0,0); (1;1) и т.д.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Сумма элементов главной диагонали: 1+9+2 = 12
+
+int numRows = SetNumber("Rows ");
+int numColumns = SetNumber("Columns ");
+
+int numMaxValue = SetNumber("Max number ");
+int numMinValue = SetNumber("Min number ");
+
+var matrix = GetMatrix(numRows, numColumns, numMinValue, numMaxValue);
+Print(matrix);
+System.Console.WriteLine();
+System.Console.WriteLine(DigitsSumm(matrix));
+
+
+int SetNumber(string numberName)
+{
+    Console.Write($"Enter number {numberName}: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+int[,] GetMatrix(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int l = 0; l < columns; l++)
+        {
+            matrix[i, l] = new Random().Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void Print(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int l = 0; l < matrix.GetLength(1); l++)
+        {
+            System.Console.Write(matrix[i, l] + " ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+void CheckValue(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int l = 0; l < matrix.GetLength(1); l++)
+        {
+            if (isEven(i, l))
+            {
+                matrix[i, l] *= matrix[i, l];
+            }
+        }
+
+    }
+}
+
+bool isEven(int i, int l)
+{
+    return i == l;
+}
+
+int DigitsSumm(int[,] matrix)
+{
+    int Sum = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int l = 0; l < matrix.GetLength(1); l++)
+        {
+            if (isEven(i, l))
+            {
+                Sum += matrix[i, l];
+            }
+        }
+    }
+    return Sum;
+}
+
+
